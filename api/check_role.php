792cb6082029b27d68866e0683ae825addb8e200
LgZika <luizgustavodiaspulzoficial@gmail.com>
2026-02-26 23:51:50 -0300

    chore(api): remove debug script check_role.php

--- api/check_role.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-\QueueMaster\Core\Database::getInstance()->connect();
-$user = \QueueMaster\Models\User::findByEmail('luizg.pulz@gmail.com');
-echo "CURRENT ROLE FOR luizg.pulz@gmail.com: " . ($user['role'] ?? 'NOT FOUND') . "\n";
